9e32a1cb268a22b5e7e8a25701f84c6387e85095
Ramesh <ramesh.nagaraj21@gmail.com>
2015-11-28 20:21:42 +0530

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
+  
   devise_for :users
   resources :links do
     member do
       put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
 
   root 'links#index'
@@ -43,8 +46,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
