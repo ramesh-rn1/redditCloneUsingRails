a084fdd55abf8e7aca00b9c1e7be22f867b5bfb4
Ramesh <ramesh.nagaraj21@gmail.com>
2015-11-29 11:02:00 +0530

    Added name field and minor tweaks

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,7 @@
 class Link < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
-	has_many :comments
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true
+	validates :url, presence: true
 end
